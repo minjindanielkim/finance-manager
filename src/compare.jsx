7b0dd71ca24bc584c2a1d814a16a3daba4b744bb
minjindanielkim <kimdani@aquinas.org>
2024-07-19 15:11:21 +0900

    added a second option to add a csv file

--- src/compare.jsx
+++ src/compare.jsx
@@ -3,16 +3,27 @@ import CSVReader from "react-csv-reader";
 
 const CompareCSV = () => {
     const [dataCSV, setData] = useState([]); 
-    const readData = (data) => {
-        console.log(data)
-        let arr = [];
-        
-        console.log(arr);
+    const [secondData, setSecond] = useState([]);
+
+    const readData1 = (data) => {
+        for(let i = 0; i < data.length; i++) {
+            dataCSV.push(data[i][1])
+        }
     }
+
+    const readData2 = (data) => {
+        for(let i = 0; i < data.length; i++) {
+            secondData.push(data[i][1])
+        }
+    }
+
     return (
         <>
         <h1>Compare CSV Files</h1>
-        <CSVReader onFileLoaded={(data, fileInfo, originalFile) => readData(data)} />
+        <CSVReader onFileLoaded={(data) => readData1(data)} />
+        <div>
+            <CSVReader onFileLoaded={(data) => readData2(data)} />
+        </div>
         </>
     )
 }
