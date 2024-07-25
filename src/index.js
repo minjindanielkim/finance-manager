8973cb147d4f2e84181dc2b92f74677fbf7bacf8
minjindanielkim <kimdani@aquinas.org>
2024-07-25 17:03:28 +0900

    added vercel analytics

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ClerkProvider } from '@clerk/clerk-react';
+import { Analytics } from "@vercel/analytics/react"
 
 const publicKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY
 
@@ -11,6 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ClerkProvider publishableKey={publicKey}>
     <App />
+    <Analytics />
   </ClerkProvider>
 );
 
